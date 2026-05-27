6c35d00623e45e3d8ef1ae7c5deb5d2659d43ebb
MD ABDUR RAHMAN SHOAIB <shoaibontheway@gmail.com>
2026-05-27 13:03:49 +0600

    (advanced) Mathematical operation shorthand

---
+++ shorthand.js
@@ -0,0 +1,21 @@
+var price = 54;
+// price = price + 6;
+price += 6
+// console.log(price);
+
+var fridge = 45000;
+// fridge = fridge - 5000;
+fridge -= 5000
+// console.log(fridge);
+
+var orangePrice = 30;
+// orangePrice = orangePrice *3;
+orangePrice *= 3
+// console.log(orangePrice);
+
+
+const first = `mobarak`;
+const second = `tobarak`;
+
+const fullName = first + second;
+console.log(fullName);
