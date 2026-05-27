cc5f62f7416786bc9cf5176013e234d19fe56ae7
MD ABDUR RAHMAN SHOAIB <shoaibontheway@gmail.com>
2026-05-27 12:51:45 +0600

    JavaScript Keyword, Variable name naming convention

---
+++ number.js
@@ -0,0 +1,16 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+var applePrice = parseInt("25");
+
+// console.log(typeof orangePrice);
+// console.log(typeof applePrice);
+// console.log(orangePrice + applePrice);
+// console.log(orangePrice);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+// console.log(parseFloat(total.toFixed(5)));
+
+const num = -50 / 0;
+console.log(num);
