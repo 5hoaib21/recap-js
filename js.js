2e2c106a1e3329fe40f63205b39ac6f45c4f6490
MD ABDUR RAHMAN SHOAIB <shoaibontheway@gmail.com>
2026-05-27 11:46:37 +0600

    start now

---
+++ js.js
@@ -0,0 +1,12 @@
+// const cars = [];
+// cars[0]= ;
+// cars[1]= ;
+// cars[2]= ;
+// 
+// console.log('List of cars',cars);
+
+const cars = ['Toyota', 'BMW', 'Volvo'];
+// console.log({cars});
+
+console.log(typeof cars);
+// document.write(5=10)
