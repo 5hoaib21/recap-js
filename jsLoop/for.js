a77d3182975c89a885cd337743fb4e791d21b91e
MD ABDUR RAHMAN SHOAIB <shoaibontheway@gmail.com>
2026-05-29 08:31:34 +0600

    Problem solving with For Loop

---
+++ jsLoop/for.js
@@ -0,0 +1,41 @@
+// let num = 0;
+// while (num < 5) {
+//   console.log(num);
+//   num++;
+// }
+
+// for (first part ; second part ; third part) {
+//loop statement
+// }
+
+// for(let num = 0; num < 5; num++){
+//     console.log(num);
+    
+// }
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// for(let i = 1; i <= 5; i++){
+//     console.log(i);
+// }
+
+//even
+// for(let i = 0; i<=20; i = i + 2){
+//     console.log(i);
+// }
+
+// for(let i = 1; i <=20; i = i + 2){
+//     console.log(i);
+// }
+
+// for(let i = 11; i <= 11; i--){
+// // console.log(i);
+// }
+
+
+// for(let i = 10; i >= 0; i--){
+//     console.log(i);
+    
+// }
