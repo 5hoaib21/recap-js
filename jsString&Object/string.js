67305c5df38e1a207511059ee23f5e2628b16cab
MD ABDUR RAHMAN SHOAIB <shoaibontheway@gmail.com>
2026-05-29 12:16:32 +0600

    Introduction to string and string vs array

---
+++ jsString&Object/string.js
@@ -0,0 +1,15 @@
+const country = 'Bangladesh';
+const division = 'Barishal';
+const district = 'Bhola';
+const thana = new String('Daulatkhan');
+
+// console.log('country',typeof country);
+// console.log('division',typeof division);
+// console.log('district',typeof district);
+// console.log('thana',typeof thana);
+// console.log('thana', thana);
+
+
+
+const nums = [2,165,46,44,45,47,431];
+console.log(nums.length);
